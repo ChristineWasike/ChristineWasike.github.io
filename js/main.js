157292a9084fbf5c6120bcee83d94ee3ef8214e0
Christine Wasike <christinewasike22@gmail.com>
2017-10-21 11:12:29 +0300

    adding an onclick to the hamburger menu

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$document.ready(function () {
+    $(".button-collapse").sideNav();
+
+});
+
+// function myNav() {
+//     $(".button-collapse").sideNav();
+//     function myNav() {
+//         alert("MyNav")
+//     }
+// }
