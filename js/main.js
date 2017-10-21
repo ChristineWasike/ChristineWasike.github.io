9350a0a1af851df3c3b6abae96446d1254c3b6a0
Christine Wasike <christinewasike22@gmail.com>
2017-10-21 17:23:22 +0300

    Working on some smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
+// Business Logic
+
+window.addEventListener("load", function() { window. scrollTo(0, 0); });
+
+// User interface
+
 $document.ready(function () {
     $(".button-collapse").sideNav();
 
 });
-
-// function myNav() {
-//     $(".button-collapse").sideNav();
-//     function myNav() {
-//         alert("MyNav")
-//     }
-// }
+$('#smooth, #projects').on('click', function(e) {
+    e.preventDefault();
+    var target = $(this).get(0).id == 'smooth' ? $('#projects') : $('#smooth');
+    // $('html, body').stop().animate({
+    //   scrollTop: target.offset().top
+    // }, 1000);
+  });
