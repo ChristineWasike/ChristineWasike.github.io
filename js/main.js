229d462b0ce68546e60313fe7b3109c6a36a612b
Christine Wasike <christinewasike22@gmail.com>
2017-11-01 12:25:12 +0300

    Styling banner

--- js/main.js
+++ js/main.js
@@ -8,16 +8,3 @@ window.addEventListener("load", function () {
 
 $(".button-collapse").sideNav();
 
-$(".navLink").on('click', function (event) {
-    if (this.hash !== "") {
-        event.preventDefault();
-
-        var hash = this.hash;
-
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 800, function () {
-            window.location.hash = hash;
-        });
-    }
-});
